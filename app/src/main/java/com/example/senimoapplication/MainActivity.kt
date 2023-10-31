e5cb71b857d3ea499ef4b02acbdf41d1957ae9ee
julie-jihyelee <jh15693@naver.com>
2023-10-31 19:27:20 +0900

    Splash add

--- app/src/main/java/com/example/senimoapplication/MainActivity.kt
+++ app/src/main/java/com/example/senimoapplication/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.senimoapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+
+    val bnvMain : BottomNavigationView = findViewById(R.id.bnvMain)
+
+
+
+
+    }
+
   }
-}
