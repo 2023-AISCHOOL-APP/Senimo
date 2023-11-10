18dc5202a73e96ad3bca7ec88e3bca65ae28907e
heejun8250 <gmlwns262@naver.com>
2023-11-10 22:01:31 +0900

    feat: Server 파일 업로드 Server폴더 생성 -> node.js로 서버 생성 + MySQL DB와 연결

---
+++ Server/config/database.js
@@ -0,0 +1,20 @@
+const mysql = require('mysql2')
+
+let conn = mysql.createConnection({
+  'host': 'project-db-stu3.smhrd.com',
+  'user': 'Insa4_App_final_3',
+  'password': 'aischool3',
+  'port': 3307,
+  'database': 'Insa4_App_final_3'
+})
+
+conn.connect(error => {
+  if (error) {
+    console.error("데이터베이스 연결 실패: " + error.stack);
+    return;
+  }
+  console.log('데이터베이스에 연결되었습니다.');
+})
+
+// 내 mysql 정보를 가지고 연결한 conn을 모듈화
+module.exports = conn;
