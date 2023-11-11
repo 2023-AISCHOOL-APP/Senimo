2a432e9269adc8d56543491f3c5cd41f016ca2c4
heejun8250 <gmlwns262@naver.com>
2023-11-11 18:01:03 +0900

    test: 쿼리문 테스트

--- Server/routes/appMain.js
+++ Server/routes/appMain.js
@@ -4,14 +4,21 @@ const conn = require('../config/database');
 
 router.get('/', (req, res) => {
   console.log('result', req.body);
-  // SQL 쿼리를 실행합니다.
-  // const query = `
-  // select A.club_name, A.club_introduce, A.max_cnt, A.club_gu, k.keyword_name
-  // from tb_club A
-  // left join tb_club_keyword B on A.club_code = B.club_code
-  // left join tb_keyword C on B.keyword_code = C.keyword_code`;
 
-  const query = `select * from tb_user`
+  const query = `
+    select c.club_name, c.club_introduce, c.max_cnt, k.keyword_name 
+    from tb_club c 
+    left join tb_club_keyword ck on c.club_code = ck.Club_code 
+    left join tb_keyword k on ck.keyword_code = k.keyword_code
+  `
+
+  // const query = `select * from tb_user`
+
+  // const query = `
+  //   select c.* from tb_interest_club ic
+  //   join tb_club c on ic.club_code = c.club_code
+  //   where ic.user_kakao_token = 'user2'
+  // `
 
   conn.query(query, (err, rows) => {
     console.log('rows :', rows);
