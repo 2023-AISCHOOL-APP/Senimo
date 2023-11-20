6daba2b1cb9366851e468a3bdeb3de40b36f0afd
hyojung22 <137032079+hyojung22@users.noreply.github.com>
2023-11-20 19:31:41 +0900

    feat: 회원가입 페이지 수정
    회원 가입 페이지
    - ID 중복, pw 일치/불일치 시 warning메세지 추가
    - 비밀번호와 비밀번호 확인 필드에 TextWatcher 추가
      -> 일치하면 "비밀번호 일치"
      -> 불일치하면 "비밀번호 일치하지 않습니다"
    - 아이디 중복체크
      -> success / dup / error 로 나눔
      -> success 시 "사용 가능한 ID입니다"
      -> dup시 "중복된 아이디입니다"
      -> error시 "에러가 발생했습니다  (  error는 Toast창으로 띄우기 )

--- app/src/main/java/com/example/senimoapplication/Login/Activity_login/SignUpActivity.kt
+++ app/src/main/java/com/example/senimoapplication/Login/Activity_login/SignUpActivity.kt
@@ -3,7 +3,10 @@ package com.example.senimoapplication.Login.Activity_login
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
+import android.view.View
 import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
 import com.example.senimoapplication.Login.VO.SignUpResVO
@@ -13,6 +16,7 @@ import com.example.senimoapplication.server.Server
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import kotlin.io.path.PathWalkOption
 
 class SignUpActivity : AppCompatActivity() {
 
@@ -66,6 +70,43 @@ class SignUpActivity : AppCompatActivity() {
       startActivity(intent)
       finish()
     }
+
+    // 비밀번호와 비밀번호 확인 필드에 TextWatcher 추가
+    val passwordTextWatcher = object : TextWatcher {
+      override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+        // 텍스트가 변경되기 전에 호출
+      }
+
+      override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+        // 텍스트가 변경될 때마다 호출
+        checkPasswordsMatch()
+      }
+
+      override fun afterTextChanged(s: Editable?) {
+        // 텍스트가 변경된 후에 호출
+      }
+    }
+    binding.etSignUpPw.addTextChangedListener(passwordTextWatcher)
+    binding.etSignUpPwCheck.addTextChangedListener(passwordTextWatcher)
+  }
+
+  private fun checkPasswordsMatch() {
+    val password = binding.etSignUpPw.text.toString()
+    val confirmPassword = binding.etSignUpPwCheck.text.toString()
+
+    if (password.isNotEmpty() && confirmPassword.isNotEmpty()) {
+      if (password == confirmPassword) {
+        binding.tvUserPwWarning.visibility = View.VISIBLE
+        binding.tvUserPwWarning.text = "비밀번호가 일치합니다."
+        binding.tvUserPwWarning.setTextColor(getResources().getColor(R.color.Pass))
+      } else {
+        binding.tvUserPwWarning.visibility = View.VISIBLE
+        binding.tvUserPwWarning.text = "비밀번호가 일치하지 않습니다."
+        binding.tvUserPwWarning.setTextColor(getResources().getColor(R.color.main))
+      }
+    } else {
+      binding.tvUserPwWarning.visibility = View.GONE
+    }
   }
 
   fun checkUserId(userId: String){
@@ -75,15 +116,31 @@ class SignUpActivity : AppCompatActivity() {
     call.enqueue(object : Callback<SignUpResVO> {
       override fun onResponse(call: Call<SignUpResVO>, response: Response<SignUpResVO>) {
         Log.d("checkId", response.toString())
-        if (response.isSuccessful) {
-          val checkIdRes = response.body()
-          if (checkIdRes != null && checkIdRes.rows == "success") {
-            Toast.makeText(this@SignUpActivity, "사용가능한 아이디입니다.", Toast.LENGTH_SHORT).show()
-          } else {
-            Toast.makeText(this@SignUpActivity, "중복된 아이디입니다.", Toast.LENGTH_SHORT).show()
-            binding.etSignUpId.text.clear()
+
+
+        val checkIdRes = response.body()
+        if (checkIdRes != null) {
+          when (checkIdRes.rows) {
+            "success" -> {
+              binding.tvUserIdWarning.visibility = View.VISIBLE
+              binding.tvUserIdWarning.text = "사용 가능한 ID입니다."
+              binding.tvUserIdWarning.setTextColor(getResources().getColor(R.color.Pass))
+            }
+            "dup" -> {
+              binding.tvUserIdWarning.visibility = View.VISIBLE
+              binding.tvUserIdWarning.text = "중복된 아이디입니다."
+              binding.tvUserIdWarning.setTextColor(getResources().getColor(R.color.main))
+              binding.etSignUpId.text.clear()
+            }
+            "error" -> {
+              Toast.makeText(this@SignUpActivity, "에러가 발생했습니다.", Toast.LENGTH_SHORT).show()
+            }
           }
+          Log.d("checkId", "Rows: ${checkIdRes.rows}")
+        } else {
+          Log.d("checkId", "응답이 null입니다.")
         }
+
       }
 
       override fun onFailure(call: Call<SignUpResVO>, t: Throwable) {
