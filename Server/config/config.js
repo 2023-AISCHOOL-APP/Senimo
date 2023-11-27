2223bbac4c5a1d18734a430bb184ff6daa3e29ff
heejun8250 <gmlwns262@naver.com>
2023-11-27 16:46:57 +0900

    chore : 개인 ip, 이미지 경로 사용

--- Server/config/config.js
+++ Server/config/config.js
@@ -1,7 +1,7 @@
 const config = {
-    baseURL: 'https://improved-sadly-snake.ngrok-free.app'
+    // baseURL: 'https://improved-sadly-snake.ngrok-free.app'
     //baseURL: 'http://115.95.222.206:5555'
-    //baseURL: 'http://192.168.70.205:3333'
+    baseURL: 'http://192.168.70.69:3333' // 희준IP
     //baseURL: 'http://192.168.70.207:3333'
     //baseURL: 'http://192.168.35.13'
   };
