c29a23acf3c4b1e3a6139cab019177815aa461d3
hyojung22 <137032079+hyojung22@users.noreply.github.com>
2023-11-28 12:36:34 +0900

    feat: 회원 탈퇴 페이지
    - 회원 탈퇴 동의합니다 클릭 유무 -> 탈퇴하기 버튼 활성화/비활성화
    - 체크 아이콘 추가

--- app/src/main/java/com/example/senimoapplication/MainPage/Activity_main/MemberDropOutActivity.kt
+++ app/src/main/java/com/example/senimoapplication/MainPage/Activity_main/MemberDropOutActivity.kt
@@ -1,6 +1,7 @@
 package com.example.senimoapplication.MainPage.Activity_main
 
 import android.content.Intent
+import android.graphics.drawable.BitmapDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
@@ -12,6 +13,8 @@ import com.example.senimoapplication.databinding.ActivityMemberDropOutBinding
 class MemberDropOutActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMemberDropOutBinding
+    var isChecked = false
+    var isMainBackground = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_member_drop_out)
@@ -20,11 +23,30 @@ class MemberDropOutActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
+        // 초기 상태 설정
+        binding.imgMCheck.setImageResource(R.drawable.ic_checkbox_gray30)
+        binding.btnSetDropout.setBackgroundResource(R.drawable.button_shape_gray30)
+
+        binding.imgMCheck.setOnClickListener {
+            if (isChecked) {
+                binding.imgMCheck.setImageResource(R.drawable.ic_checkbox_gray30)
+                binding.btnSetDropout.setBackgroundResource(R.drawable.button_shape_gray30)
+                isMainBackground = false
+            } else {
+                binding.imgMCheck.setImageResource(R.drawable.ic_checkbox_color)
+                binding.btnSetDropout.setBackgroundResource(R.drawable.button_shape_main)
+                isMainBackground = true
+            }
+            isChecked = !isChecked
+        }
+
         binding.btnSetDropout.setOnClickListener {
-            val intent = Intent(this@MemberDropOutActivity,IntroActivity::class.java)
-            startActivity(intent)
-            finish()
-            Toast.makeText(this@MemberDropOutActivity,"탈퇴되었습니다.",Toast.LENGTH_SHORT).show()
+            if(isMainBackground) {
+                val intent = Intent(this@MemberDropOutActivity,IntroActivity::class.java)
+                startActivity(intent)
+                finish()
+                Toast.makeText(this@MemberDropOutActivity,"탈퇴되었습니다.",Toast.LENGTH_SHORT).show()
+            }
 
         }
 
