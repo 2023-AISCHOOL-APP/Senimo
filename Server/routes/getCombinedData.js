53fe779f68a97ffeeed3e4ca0cf099a274e160ef
julie-jihyelee <jh15693@naver.com>
2023-11-29 20:16:17 +0900

    fix : 내모임 참석자 인원 조회 코드 수정
    - 내모임 데이터 쿼리문 수정

--- Server/routes/getCombinedData.js
+++ Server/routes/getCombinedData.js
@@ -8,31 +8,30 @@ router.get('/getCombinedData/:user_id', (req, res) => {
 
   // 모임 일정 가져오기: 기존 getSchedules쿼리 -> ScheduleVO
   const myScheduleQuery = `
-  SELECT s.sche_code, s.club_code, s.sche_title, s.sche_content, s.sche_date, s.sche_location, s.fee, s.max_num, COUNT(j.user_id) AS joined_members,
-  CONCAT('${config.baseURL}/uploads/', s.sche_img) AS sche_img,a.club_name
-  FROM tb_schedule s 
-  LEFT JOIN tb_sche_joined_user j ON s.sche_code = j.sche_code
-  join tb_club a on s.club_code = a.club_code
-  WHERE j.user_id = ?
-  GROUP BY s.sche_code, s.club_code, s.sche_title, s.sche_content, s.sche_date, s.sche_location, s.fee, s.max_num, s.sche_img;;
-  `
+  SELECT s.sche_code, s.club_code, s.sche_title, s.sche_content, s.sche_date, s.sche_location, s.fee, 
+    s.max_num, (SELECT COUNT(*) FROM tb_sche_joined_user WHERE sche_code = s.sche_code) AS joined_members, CONCAT('${config.baseURL}/uploads/', s.sche_img) AS sche_img,a.club_name
+    FROM tb_schedule s 
+    JOIN tb_club a ON s.club_code = a.club_code
+    WHERE s.sche_code IN (SELECT sche_code FROM tb_sche_joined_user WHERE user_id = ?)
+    GROUP BY s.sche_code, s.club_code, s.sche_title, s.sche_content, s.sche_date, s.sche_location, s.fee, s.max_num, s.sche_img, a.club_name    
+  `;
 
   // 내가 가입한 모임 가져오기 : 기존 getMeetings 쿼리 -> MeetingVO
   const myClubQuery = `
-  SELECT c.club_code, c.club_location, c.club_name, c.club_introduce, COUNT(j.user_id) AS attend_user_cnt, c.max_cnt, c.club_img,
-  k.keyword_name, j.user_id
-  FROM tb_club c LEFT JOIN tb_keyword k ON c.keyword_code = k.keyword_code
-  LEFT JOIN tb_join j ON c.club_code = j.club_code
-  where j.user_id = ?
-  GROUP BY 
-  c.club_code, c.club_location, c.club_name, c.club_introduce, c.max_cnt, c.club_img, k.keyword_name, c.club_img
-  `
+  SELECT c.club_code, c.club_location, c.club_name, c.club_introduce, 
+    (SELECT COUNT(*) FROM tb_join WHERE club_code = c.club_code) AS attend_user_cnt, c.max_cnt, 
+    c.club_img,k.keyword_name
+  FROM tb_club c 
+  LEFT JOIN tb_keyword k ON c.keyword_code = k.keyword_code
+  WHERE c.club_code IN (SELECT club_code FROM tb_join WHERE user_id = ?)
+  GROUP BY c.club_code, c.club_location, c.club_name, c.club_introduce, c.max_cnt, c.club_img, k.keyword_name
+  `;
   
 
   // 내 관심 모임 가져오기 : MeetingVO
   const myInterestQuery = `
     SELECT c.club_location, c.club_name, c.club_introduce, k.keyword_name,
-      COUNT(j.user_id) AS participant_count, c.max_cnt, c.club_img, c.club_code, ic.user_id
+      COUNT(j.user_id) AS attend_user_cnt, c.max_cnt, c.club_img, c.club_code, ic.user_id
     FROM tb_interested_club ic
     JOIN tb_club c ON ic.club_code = c.club_code
     JOIN tb_keyword k ON c.keyword_code = k.keyword_code
